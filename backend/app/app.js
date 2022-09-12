60cb2d45487626bb4a6ec50416815bf0dd268623
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-09-12 15:24:44 -0300

    Refactor: clear app

--- backend/app/app.js
+++ backend/app/app.js
@@ -2,7 +2,6 @@ require("express-async-errors");
 
 const express = require("express");
 const app = express();
-const wss = require("./websocket");
 const http = require("http");
 const server = http.createServer(app);
 
