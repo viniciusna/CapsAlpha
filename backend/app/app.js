9749c265466bcc072de197af305070314ae0d2dc
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-09-12 15:19:54 -0300

    Refactor: change structure of websokcet

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,20 +1,10 @@
 require("express-async-errors");
-//const indexRoute = require("./routes/index");
-const { wsStart } = require("./websocket/index");
-const uuid = require("./utils/uuid");
 
 const express = require("express");
 const app = express();
-
+const wss = require("./websocket");
 const http = require("http");
 const server = http.createServer(app);
-
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({
-  server: server,
-  clientTracking: true,
-});
-const WebsocketMethods = require("./websocket/methods");
 
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -47,9 +37,6 @@ app.use(cookieParser());
 // };
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
-////////////////////////////////////////////////////////////////////////////////
-// HTTP Server
-
 app.use("/", indexRoute);
 app.use("/user", userRoute);
 app.use("/document", documentRoute);
@@ -70,91 +57,4 @@ app.use((err, req, res, next) => {
   next();
 });
 
-////////////////////////////////////////////////////////////////////////////////
-// WebSocket Server
-
-wss.on("error", (error) => {
-  console.log("Socket Server Error: " + error);
-});
-
-wss.on("close", () => {
-  console.log("Socket Server Close");
-});
-
-wss.on("connection", (ws, req) => {
-  console.log("Socket Client Connection");
-
-  ws.on("error", (error) => {
-    console.log("Socket Error: " + error);
-  });
-
-  ws.on("pong", () => {
-    ws.isAlive = true;
-  });
-
-  ws.on("close", () => {
-    const websocketMethods = new WebsocketMethods(ws);
-    websocketMethods.removeUser(ws.room, ws.userId);
-    console.log("Socket Client Disconnected");
-  });
-
-  //wsStart(ws, req)
-  ws.userId = uuid();
-
-  ws.on("message", async (data) => {
-    try {
-      const websocketMethods = new WebsocketMethods(ws);
-      const obj = JSON.parse(data);
-      console.log(data);
-      console.log(obj);
-      const type = obj.type;
-      const params = obj.params;
-      switch (type) {
-        case "join":
-          await websocketMethods.join(params);
-          break;
-        case "leave":
-          websocketMethods.leave(params);
-          break;
-        case "message":
-          websocketMethods.message(params, wss.clients, WebSocket);
-          break;
-        default:
-          console.warn(`Type: ${type} unknown`);
-          break;
-      }
-    } catch (e) {
-      console.log(
-        "WS-MESSAGE ERROR: message(fn) --> received non-parsable DATA --> " + e
-      );
-      return;
-    }
-  });
-
-  //ws.on('message', (data, isBinary) => { })
-});
-
-////////////////////////////////////////////////////////////////////////////////
-// Export HTTP Server
-
 module.exports = server;
-
-// {
-//   api_caps_alpha_1_1  |       _events: [Object],
-//   api_caps_alpha_1_1  |       _eventsCount: 4,
-//   api_caps_alpha_1_1  |       _binaryType: 'nodebuffer',
-//   api_caps_alpha_1_1  |       _closeCode: 1006,
-//   api_caps_alpha_1_1  |       _closeFrameReceived: false,
-//   api_caps_alpha_1_1  |       _closeFrameSent: false,
-//   api_caps_alpha_1_1  |       _closeMessage: [Object],
-//   api_caps_alpha_1_1  |       _closeTimer: null,
-//   api_caps_alpha_1_1  |       _extensions: {},
-//   api_caps_alpha_1_1  |       _paused: false,
-//   api_caps_alpha_1_1  |       _protocol: '',
-//   api_caps_alpha_1_1  |       _readyState: 1,
-//   api_caps_alpha_1_1  |       _receiver: [Object],
-//   api_caps_alpha_1_1  |       _sender: [Object],
-//   api_caps_alpha_1_1  |       _socket: [Object],
-//   api_caps_alpha_1_1  |       _isServer: true,
-//   api_caps_alpha_1_1  |       userId: '4909fe0e-8f05-4f21-86b1-f66e14b02673'
-//   api_caps_alpha_1_1  |     }
