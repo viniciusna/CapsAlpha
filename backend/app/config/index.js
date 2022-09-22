2d85302efdba75e148dd3c6780765d4ad63feb00
viniciusna <loki@LAPTOP-E0COG5FP.localdomain>
2022-09-22 20:21:48 -0300

    q q o vitor arrumou kkk

--- backend/app/config/index.js
+++ backend/app/config/index.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 
 const config = {
-  port: process.env.PORT,
-  jwt_secret: process.env.JWT_SECRET,
+  port: 3001,
+  jwt_secret: "dfevdfvsdfvweffvdfv",
   postgres: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "1978",
+    database: "caps_alpha",
   },
   redis: {
-    password: process.env.REDIS_PASSWORD,
-    host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PORT,
+    password: "",
+    host: "localhost",
+    port: 6379,
   },
 };
 
