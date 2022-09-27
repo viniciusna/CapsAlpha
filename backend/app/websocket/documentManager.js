ee28f417581683a19cc8fbca034116ea3bff5e5f
SaruMan64 <yete.abuna@gmail.com>
2022-09-27 18:27:23 -0300

    console.log delta op

--- backend/app/websocket/documentManager.js
+++ backend/app/websocket/documentManager.js
@@ -55,8 +55,11 @@ class DocumentManager {
 
   toPlaintext(delta) {
     return delta.reduce(function (text, op) {
-      if (!op.insert)
+      if (!op.insert) {
+        console.log('delta:', delta)
+        console.log('op:', op)
         throw new TypeError("only `insert` operations can be transformed!");
+      }
       if (typeof op.insert !== "string") return text + " ";
       return text + op.insert;
     }, "");
